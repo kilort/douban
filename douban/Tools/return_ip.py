27d1a7bb449013c816fcaa7fd1037139666a4d28
kilort <hl19951127>
2018-02-02 13:04:42 +0800

    douban

--- douban/Tools/return_ip.py
+++ douban/Tools/return_ip.py
@@ -2,7 +2,7 @@ import random,linecache
 
 def product_xiongmao_ip():
     #返回熊猫代理的ip
-    ip = linecache.getline("/home/hl/桌面/docker_share/share_ip.txt",random.randrange(1,11))
+    ip = linecache.getline("/home/my_Tools/share_ip.txt",random.randrange(1,11))
     proxy = "http://"+str(ip)
     print(proxy)
     return proxy
